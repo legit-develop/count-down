06bb797d37596c8a92b78805ed9e917823adce42
Sifiso Dhlamini <sb.dhlamini@gmail.com>
2023-02-17 17:57:52 +0200

    counter elements and functionality

--- index.js
+++ index.js
@@ -1,15 +1,29 @@
-const newYears = "1 Jan 2024";
+//Elements
+const daysEl = document.getElementById("days");
+const hoursEl = document.getElementById("hours");
+const minsEl = document.getElementById("mins");
+const secondsEl = document.getElementById("seconds");
+
+const newYears = "16 Dec 2023";
 
 function countDown() {
   const newYearsDate = new Date(newYears);
   const currentDate = new Date();
 
-  const seconds = (newYearsDate - currentDate) * 1000;
-  console.log(seconds);
+  const totalSeconds = (newYearsDate - currentDate) / 1000;
+  //console.log(totalSeconds);
 
-  console.log(newYearsDate - currentDate);
+  const days = Math.floor(totalSeconds / 3600 / 24);
+  const hours = Math.floor(totalSeconds / 3600) % 24;
+  const minutes = Math.floor(totalSeconds / 60) % 60;
+  const seconds = Math.floor(totalSeconds % 60);
+
+  daysEl.innerText = days;
+  hoursEl.innerText = hours;
+  minsEl.innerText = minutes;
+  secondsEl.innerText = seconds;
 }
 
 countDown();
 
-setInterval(countDown, 1000);
+//setInterval(countDown, 1000);
